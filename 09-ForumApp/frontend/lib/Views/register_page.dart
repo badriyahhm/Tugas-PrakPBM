e5c31c15ae0a4308f129619c82c5aae225502407
badriyahhm <badriyahhusnayah@gmail.com>
2024-06-03 14:13:05 +0700

    membuat halaman register

--- 09-ForumApp/frontend/lib/Views/register_page.dart
+++ 09-ForumApp/frontend/lib/Views/register_page.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:tugas8/Views/input_widget.dart';
+import 'package:tugas8/Views/login_page.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:tugas8/Views/register_page.dart';
+import 'package:tugas8/Views/input_widget.dart';
 import 'package:get/get.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<RegisterPage> createState() => _RegisterPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -21,31 +23,41 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20,
-          ),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                'Login Page',
+                'Register Page',
                 style: GoogleFonts.poppins(
                   fontSize: size * 0.080,
                 ),
               ),
-              SizedBox(height: 30),
+              const SizedBox(height: 30),
+              InputWidget(
+                hintText: 'Name',
+                controller: _nameController,
+                obscureText: false,
+              ),
+              const SizedBox(height: 20),
+              InputWidget(
+                hintText: 'Username',
+                controller: _usernameController,
+                obscureText: false,
+              ),
+              const SizedBox(height: 30),
               InputWidget(
                 hintText: 'Email',
-                obscureText: false,
                 controller: _emailController,
+                obscureText: false,
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               InputWidget(
                 hintText: 'Password',
-                obscureText: true,
                 controller: _passwordController,
+                obscureText: true,
               ),
-              SizedBox(height: 30),
+              const SizedBox(height: 30),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.black,
@@ -55,18 +67,19 @@ class _LoginPageState extends State<LoginPage> {
                 ),
                 onPressed: () {},
                 child: Text(
-                  'Login',
+                  'Register',
                   style: GoogleFonts.poppins(
                     fontSize: size * 0.040,
                   ),
                 ),
               ),
+              const SizedBox(height: 20),
               TextButton(
                 onPressed: () {
-                  Get.to(() => const RegisterPage());
+                  Get.to(() => const LoginPage());
                 },
                 child: Text(
-                  'Register',
+                  'Login',
                   style: GoogleFonts.poppins(
                     fontSize: size * 0.040,
                     color: Colors.black,
